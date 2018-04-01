accad7d24f83e5ea282a4fd135e73b3bd42145e8
wutian <wutian@staff.weibo.com>
2018-04-02 00:00:41 +0800

    Update update_static_lib script

--- update_static_lib.sh
+++ update_static_lib.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+CONFIGURATION="Release"
 BASE_DIR=`dirname $0`
 FRAMEWORK_PROJECT_PATH="${BASE_DIR}/Example/CronetFramework/"
 DERIVED_DATA_PATH="${FRAMEWORK_PROJECT_PATH}/DerivedData"
-XCODE_OUTPUT_LIB_PATH="${DERIVED_DATA_PATH}/Build/Products/Release/CronetNetworking/libCronetNetworking.a"
+XCODE_OUTPUT_LIB_PATH="${DERIVED_DATA_PATH}/Build/Products/${CONFIGURATION}/CronetNetworking/libCronetNetworking.a"
 RESULT_LIB_PATH="${BASE_DIR}/libCronetNetworking.a"
 
 rm -f $RESULT_LIB_PATH || true
 rm -f $XCODE_OUTPUT_LIB_PATH || true
 
-xcodebuild -workspace "${FRAMEWORK_PROJECT_PATH}/CronetFramework.xcworkspace" -scheme CronetFramework -configuration Release -derivedDataPath "$DERIVED_DATA_PATH"
+xcodebuild -workspace "${FRAMEWORK_PROJECT_PATH}/CronetFramework.xcworkspace" -scheme CronetFramework -configuration "$CONFIGURATION" -derivedDataPath "$DERIVED_DATA_PATH"
 
 if [ -e $XCODE_OUTPUT_LIB_PATH ]
 then
